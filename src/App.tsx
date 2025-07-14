873cd181bbd0f09d6dbbdb4588d3996c32b6f7e0
Jatin Mishra <jatinmishra27@gmail.com>
2025-07-14 17:54:40 +0530

    added config for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import ThemeSelector from "@/components/ThemeSelector";
 import LandingPage from "@/components/LandingPage";
@@ -48,12 +48,12 @@ const App = () => {
           <Toaster />
           <Sonner />
           <ThemeSelector />
-          <BrowserRouter>
+          <HashRouter> {/* ⬅️ use HashRouter */}
             <Routes>
               <Route path="/" element={<Index onGoToLanding={handleGoToLanding} />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
